553648f4450869be55298d2fd11ce28b0780707b
nukeop <12746779+nukeop@users.noreply.github.com>
2026-04-25 16:09:33 +0000

    implement discord client with login

--- src/clients/discord/discord.ts
+++ src/clients/discord/discord.ts
@@ -1,3 +1,24 @@
-export const run = () => {
-  throw new Error("Not implemented");
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+class DiscordClient extends Client {
+  constructor() {
+    super({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages,
+      ],
+    });
+  }
+
+  public async start(token: string): Promise<void> {
+    await this.login(token);
+  }
+}
+
+export const run = async () => {
+  dotenv.config();
+  const client = new DiscordClient();
+  await client.start(process.env.DISCORD_TOKEN!);
 };
