d4a5ef8895b596e810aef3b40568471625b9af1d
nukeop <12746779+nukeop@users.noreply.github.com>
2026-06-27 02:16:59 +0200

    Log registered commands

--- src/clients/discord/services/discord-commands.ts
+++ src/clients/discord/services/discord-commands.ts
@@ -1,14 +1,18 @@
+import { getLogger } from '@logtape/logtape';
 import type {
   Client,
   RESTPostAPIChatInputApplicationCommandsJSONBody,
 } from 'discord.js';
 import type { Command } from '../types';
 
+const logger = getLogger(['OpenBorys', 'Discord', 'Commands']);
+
 export class DiscordCommandsService {
   static #commands: Command[] = [];
 
   static registerCommand = (command: Command) => {
     DiscordCommandsService.#commands.push(command);
+    logger.info('Registered command: /{name}', { name: command.data.name });
   };
 
   static unregisterCommand = (name: string) => {
@@ -23,6 +27,14 @@ export class DiscordCommandsService {
   static getAllAsJSON = (): RESTPostAPIChatInputApplicationCommandsJSONBody[] =>
     DiscordCommandsService.#commands.map((command) => command.data.toJSON());
 
-  static reloadCommands = async (client: Client) =>
-    client.application?.commands.set(DiscordCommandsService.getAllAsJSON());
+  static reloadCommands = async (client: Client) => {
+    const names = DiscordCommandsService.#commands.map((c) => c.data.name);
+    logger.info('Syncing {count} commands to Discord: {names}', {
+      count: names.length,
+      names: names.join(', '),
+    });
+    await client.application?.commands.set(
+      DiscordCommandsService.getAllAsJSON(),
+    );
+  };
 }
